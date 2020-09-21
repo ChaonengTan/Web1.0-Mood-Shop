63801ef60bc9cb8b6666da31139d519f6b400ed4
Chaoneng Tan <69783753+ChaonengTan@users.noreply.github.com>
2020-09-20 19:44:30 -0700

    ++cart functional (kinda?)

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -26,46 +26,54 @@ for (let i=0; i<data.length; ++i) {
     let button = document.createElement('button')
     button.id = data[i].name
 
-      button.dataset.price = data[i].price
-      button.innerHTML = "Add to Cart"
-      newDiv.appendChild(button)
-      itemsContainer.appendChild(newDiv)
+    button.dataset.price = data[i].price
+    button.innerHTML = "Add to Cart"
+    newDiv.appendChild(button)
+    itemsContainer.appendChild(newDiv)
 
 const cart = []
-
+itemList.onchange=function(e){
+  if(e.target && e.target.classList.contains("update")){
+    const name=e.target.dataset.name
+    constqty=parseInt(e.target.value)
+    updateCart(name,qty)
+  }
+}
 itemList.onclick = function(e) {
-    if (e.target && e.target.classList.contains('remove')) {
+    if (e.target && e.target.classList.contains('remove')){
         const name = e.target.dataset.name
         removeItem(name)
-    } else if (e.target && e.target.classList.contains('remove-one')) {
-        const name = e.target.dataset.name
-        removeItem(name, 1)
-    } else if (e.target && e.target.classList.contains('add-one')) {
+    } 
+    else if (e.target && e.target.classList.contains('add-one')){
         const name = e.target.dataset.name
         addItem(name)
+    } 
+    else if (e.target && e.target.classList.contains('remove-one')){
+        const name = e.target.dataset.name
+        removeItem(name, 1)
     }
 }
-
-function addItem(name, price) {
-    for (let i = 0; i < cart.length; i += 1) {
-        if (cart[i].name === name) {
-            cart[i].qty += 1
+function addItem(name, price=0){
+    for (let i=0;i<cart.length;i++){
+        if (cart[i].name === name){
+            cart[i].qty++
             return
         }
     }
-    const item = { name, price, qty: 1 }
+    const item={name, price, qty: 1}
     cart.push(item)
 }
 
-function showItems() {
+function showItems(){
     cartQty.innerHTML = `You have ${getQty()} items in your cart`
     let itemStr = ''
-    for (let i = 0; i < cart.length; i += 1) {
+    for (let i=0;i<cart.length;i++){
         const { name, price, qty } = cart[i]
         itemStr += `<li>${name} $${price} x ${qty} = ${qty * price} 
         <button class="remove" data-name="${name}">Remove</button>
         <button class="remove-one" data-name="${name}"> - </button> 
-        <button class="add-one" data-name="${name}"> + </button> 
+        <button class="add-one" data-name="${name}"> + </button>
+        <input class"update" type="number">
         </li>`
     }
     itemList.innerHTML = itemStr
@@ -76,34 +84,43 @@ all_items_button.forEach(elt => elt.addEventListener('click', () => {
     addItem(elt.getAttribute('id'), elt.getAttribute('data-price'))
     showItems()
   }))
-function getQty() {
+function getQty(){
     let qty = 0
-    for (let i = 0; i < cart.length; i += 1) {
+    for (let i=0;i<cart.length;i++){
         qty += cart[i].qty
     }
     return qty
 }
-function getTotal() {
+function getTotal(){
     let total = 0
-    for (let i = 0; i < cart.length; i += 1) {
+    for (let i=0;i<cart.length;i++){
         total += cart[i].price * cart[i].qty
     }
     return total.toFixed(2)
 }
-function removeItem(name, qty = 0) {
-    for (let i=0; i < cart.length; i += 1) {
-        if (cart[i].name === name) {
-            if (qty > 0) {
+function removeItem(name, qty = 0){
+    for (let i=0;i<cart.length;i++){
+        if (cart[i].name === name){
+            if (qty > 0){
                 cart[i].qty -= qty
             }
-            cart[i].qty -= 1
-            if (cart[i].qty < 1 || qty === 0) {
+            cart[i].qty--
+            if (cart[i].qty < 1 || qty === 0){
                 cart.splice(i, 1)
             }
             showItems()
             return
         }
     }
+}
+function updateCart(name,qty){
+  for(let i=0;i<cart.length;i++){
+    if (cart[i].name===name){
+      cart[i].qty=qty
+      showItems()
+      return
+    }
+  }
 }
 showItems()
 }
